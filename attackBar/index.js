90ecf3d495adb108dab4a3fc0730c6bff3773ef8
Szymon Czarczynski <czarczynskiszymon@gmail.com>
2023-11-10 15:28:40 +0000

    Added cursor and trail to attack bar

--- attackBar/index.js
+++ attackBar/index.js
@@ -2,7 +2,7 @@
 // The width of the box is 63 pixels and the height is 20
 // It kinda overlaps 1 pixel with the one below/the pixel above
 //
-
+// The trail is grid locked, starting after the crit target, being 3 pixels wide, and having 5 pixel gap. Moves 8 pixels every 4 frames, or 2ppf (pixels per frame)
 
 let game = {
   canvas : document.createElement("canvas"),
@@ -21,18 +21,55 @@ function attackBar(width, height, x, y, colours) {
   this.height = height * 2;
   this.x = x;
   this.y = y;
-  ctx = game.context;
-  ctx.fillStyle = colours.dark;
-  ctx.fillRect(this.x, this.y, this.width, this.height);
-  ctx.fillStyle = "black";
-  ctx.fillRect(this.x + 2, this.y + 2, this.width - 4, this.height - 4)
-  ctx.fillStyle = colours.light;
-  ctx.fillRect(this.x + 2, this.y, 2 * 5, this.height);
-  ctx.fillStyle = "black";
-  ctx.fillRect(this.x + 4, this.y + 2, 2 * 3, this.height - 4)
-}
+  this.ctx = game.context;
+  this.distance = 256;
+  this.moving = true;
+  this.framesSincePressed = 0;
+  this.update = function () {
+    if (this.moving) {
+      this.distance -= 2;
+    } else {
+      this.framesSincePressed ++;
+    }
+    this.ctx.fillStyle = colours.dark;
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    this.ctx.fillStyle = "black";
+    this.ctx.fillRect(this.x + 2, this.y + 2, this.width - 4, this.height - 4)
+    this.ctx.fillStyle = colours.light;
+    this.ctx.fillRect(this.x + 2, this.y, 2 * 5, this.height);
+    this.ctx.fillStyle = "black";
+    this.ctx.fillRect(this.x + 4, this.y + 2, 2 * 3, this.height - 4);
 
+    // Trail
+    //
+    this.trail()
+    this.cursor()
+  }
+  this.trail = function () {
+    for (let i = Math.floor(this.distance/8)*8 -2; i < Math.floor(this.distance/8)*8 + 48; i += 8) {
+      distanceFromCursor = i - this.distance;
+      distanceFromCursor = distanceFromCursor / 32;
+      this.ctx.fillStyle = `hsla(0, 0%, 100%, ${Math.max(1 - distanceFromCursor - this.framesSincePressed/20, 0) * 0.5})`
+      this.ctx.fillRect(this.x + 16 + i*2, this.y, 2*3, this.height);
+    }
+  }
+  this.cursor = function () {
+    this.ctx.fillStyle = `hsla(0, 0%, 100%, ${Math.max(1 - this.framesSincePressed/15)})`;
+    console.log(this.ctx.fillStyle);
+    sizeModifier = this.framesSincePressed/1;
+    this.ctx.fillRect(this.distance*2 + this.x - sizeModifier/2, this.y - sizeModifier / 2, 2*3 + sizeModifier, this.height + sizeModifier);
+  }
+}
+const update = function () {
+  game.canvas.width = game.canvas.width;
+  game.canvas.height = game.canvas.height;
+  myAttackBar.update()
+  if (myAttackBar.distance < 15) {
+    myAttackBar.moving = false;
+  }
+}
 
 game.start();
 myAttackBar = new attackBar(63, 20, 80, 364, {dark: "#0503ca", light: "#139dd8"})
 
+frameUpdate = setInterval(update, 15);
